538bc49fd6fd6c88f54eec95f73064f0a6da831b
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-28 18:43:49 -0300

    Creating a ParkingServiceImpl and implements the service

---
+++ domain/service/serviceImpl/ParkingServiceImpl.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using projeto.domain.model;
+using projeto.domain.repository;
+
+namespace projeto.domain.service.serviceImpl
+{
+    public class ParkingServiceImpl : ParkingService
+    {
+
+        private ParkingRepository? repository;
+
+        public string addVehicle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Parking> listVehicle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string removeVehicle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
