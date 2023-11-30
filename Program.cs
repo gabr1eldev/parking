b99ad95241dad5c38ded67ea7ca8831b0e00d832
gabriel <gabriel.gregorio1987@gmail.com>
2023-11-30 19:37:55 -0300

    Creating case 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,50 @@
-﻿
+using projeto.domain.model;
+using projeto.domain.service.serviceImpl;
 
 
+namespace projeto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingServiceImpl service = new();
+            Parking parking = new();
+
+            Console.WriteLine("---------Wellcome to Parking!---------- \n");
+           
+
+            #pragma warning disable CS8604 // Possible null reference argument.
+            int option = int.Parse(Console.ReadLine());
+            
+            while (option != 4)
+            {
+                Console.WriteLine("[1] - Add Plate");
+                Console.WriteLine("[2] - Remove Plate");
+                Console.WriteLine("[3] - List Plates");
+                Console.WriteLine("[4] - Exit");
+                switch (option)
+                {
+                    case 1:
+                    {
+
+                        Console.WriteLine("Fixed Price");
+                        #pragma warning disable CS8604 // Possible null reference argument.
+                        var fixedPrice = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Plate car");
+                        var plateCar = Console.ReadLine();
+                        parking.SetPrice(fixedPrice);
+                        #pragma warning disable CS8604 // Possible null reference argument.
+                        parking.SetPlateCar(plateCar);
+                        service.addVehicle(parking);
+                        break;
+                        
+                    }
+                }
+            }
+
+        }   
+    }    
+}
+        
+    
--- obj/Debug/net7.0/projeto.AssemblyInfo.cs
+++ obj/Debug/net7.0/projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4b051625da1d677580a50c3b828d98bf53ab4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
